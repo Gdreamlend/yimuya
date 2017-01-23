d786dd90328c425ce6d3ec08317d23f1fa55a216
gdreamlend <gdreamlend@gmail.com>
2017-01-23 14:06:32 +0800

    add wechat

---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class WechatController extends Controller
+{
+    /**
+     * 处理微信的请求消息
+     *
+     * @return string
+     */
+    public function serve()
+    {
+
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
+            return "欢迎关注 yimuya！";
+        });
+
+        return $wechat->server->serve();
+    }
+}
